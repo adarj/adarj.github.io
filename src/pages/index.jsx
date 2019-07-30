3418dd793ff9ffe8581d3ea2b606d988ae7ea352
Akil Darjean <akildarjean@gmail.com>
2019-07-30 13:32:59 -0500

    Move Hero view to seperate file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,14 @@
 import React from "react"
+
+// Components
 import Layout from "../components/Layout"
-import LazyHero from "react-lazy-hero"
-import heroBg from "../images/hero-bg.jpg"
+
+// Views
+import Hero from "../views/Hero"
 
 export default () => (
   <>
     <Layout />
-    <LazyHero
-      imageSrc={heroBg}
-      color="#000"
-      opacity="0.6"
-      minHeight="100vh"
-    ></LazyHero>
+    <Hero></Hero>
   </>
 )
---
+++ src/views/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import LazyHero from "react-lazy-hero"
+import heroBg from "../images/hero-bg.jpg"
+
+const Hero = ({ children }) => (
+  <LazyHero imageSrc={heroBg} color="#000" opacity="0.6" minHeight="100vh">
+    {children}
+  </LazyHero>
+)
+
+export default Hero
