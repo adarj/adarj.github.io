2fe8859044027629c0565e4f58f935fea1a72c16
Akil Darjean <akildarjean@gmail.com>
2019-07-30 13:24:58 -0500

    Add initial code for Hero section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import LazyHero from "react-lazy-hero"
+import heroBg from "../images/hero-bg.jpg"
 
 export default () => (
   <>
     <Layout />
-    <div>Hello, world!</div>
+    <LazyHero
+      imageSrc={heroBg}
+      color="#000"
+      opacity="0.6"
+      minHeight="100vh"
+    ></LazyHero>
   </>
 )
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  html {
+    overflow-x: hidden;
+    box-sizing: border-box;
+  }
+  html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
   a {
     text-decoration: none;
   }
