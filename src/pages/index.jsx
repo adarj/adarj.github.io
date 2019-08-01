709de7cefac66df65b8e627d09ed2510b979859c
Akil Darjean <akildarjean@gmail.com>
2019-08-01 09:56:54 -0500

    Use react-lazy-hero directly in index file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,15 @@
 import React from "react"
+import Hero from "react-lazy-hero"
 
 // Components
 import Layout from "../components/Layout"
 
-// Views
-import Hero from "../views/Hero"
+// Images
+import heroBg from "../images/hero-bg.jpg"
 
 export default () => (
   <>
     <Layout />
-    <Hero></Hero>
+    <Hero imageSrc={heroBg} color="#000" opacity={0.6} minHeight="100vh"></Hero>
   </>
 )
