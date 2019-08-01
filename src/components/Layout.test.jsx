915d362c80ab11ae3b2ca10db23a86fa0d304fd4
Akil Darjean <akildarjean@gmail.com>
2019-08-01 15:09:30 -0500

    Use Enzyme for Layout unit test

--- src/components/Layout.test.jsx
+++ src/components/Layout.test.jsx
@@ -1,12 +1,11 @@
 import React from "react"
-import renderer from "react-test-renderer"
-import "jest-styled-components"
+import { shallow } from "enzyme"
 
 import Layout from "./Layout"
 
 describe("Layout", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<Layout />).toJSON()
+    const tree = shallow(<Layout />)
     expect(tree).toMatchSnapshot()
   })
 })
