04cf6fceb066f75e10348c5d0f388168d4082bf0
Akil Darjean <akildarjean@gmail.com>
2019-08-02 13:34:25 -0500

    Add typography.js library

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,14 @@
  */
 
 module.exports = {
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
