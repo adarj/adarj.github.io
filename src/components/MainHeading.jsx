5a98b4729bc4ec66006a572e6ba5631c7a3cef7c
Akil Darjean <akildarjean@protonmail.com>
2019-12-15 19:08:13 -0600

    Increase main heading size on large layouts

--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -4,7 +4,7 @@ import tw from 'tailwind.macro';
 
 const Title = styled.h1`${tw`text-white`};`;
 const Subtitle = styled.h2`${tw`text-white`};`;
-const Border = styled.div`${tw`border-solid border-white border-2 p-16 py-4`};`;
+const Border = styled.div`${tw`border-solid border-white border-2 lg:p-32 lg:py-8 p-16 py-4`};`;
 
 const MainHeading = () => (
   <Border>
