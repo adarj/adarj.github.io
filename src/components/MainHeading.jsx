6071390a7b32e6e104c04be2d454fe65c57a8b1b
Akil Darjean <akildarjean@protonmail.com>
2019-12-15 18:44:16 -0600

    Create border for main heading

--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -4,12 +4,13 @@ import tw from 'tailwind.macro';
 
 const Title = styled.h1`${tw`text-white`};`;
 const Subtitle = styled.h2`${tw`text-white`};`;
+const Border = styled.div`${tw`border-solid border-white border-2 p-32 py-8`};`;
 
 const MainHeading = () => (
-  <>
+  <Border>
     <Title>Akil Darjean</Title>
     <Subtitle>Software Developer</Subtitle>
-  </>
+  </Border>
 );
 
 export default MainHeading;
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,8 @@
 import Typography from 'typography';
 import moragaTheme from 'typography-theme-moraga';
 
+moragaTheme.scaleRatio = 5;
+
 const typography = new Typography(moragaTheme);
 export const { scale, rhythm, options } = typography;
 export default typography;
