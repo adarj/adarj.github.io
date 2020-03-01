ee9901b7caf6f0641d66990876e63dfbda16a25d
Akil Darjean <akildarjean@protonmail.com>
2020-03-01 15:35:32 -0600

    Reorganize tests for home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,16 +1,16 @@
 /// <reference types="cypress" />
 
-describe('Main Heading', () => {
-  it('displays correctly', () => {
+describe('Home Page', () => {
+  before(() => {
     cy.visit('/');
+  });
+
+  it('Main Heading displays correctly', () => {
     cy.get('[data-cy=title]').contains('Akil Darjean');
     cy.get('[data-cy=subtitle]').contains('Software Developer');
   });
-});
 
-describe('About Me', () => {
-  it('displays correctly', () => {
-    cy.visit('/');
+  it('About Me displays correctly', () => {
     cy.get('[data-cy=about-me-heading]').contains('About Me');
   });
 });
