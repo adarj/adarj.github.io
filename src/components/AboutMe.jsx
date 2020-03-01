39b29454e2a9315ffcd7382d725ef916cb87b674
Akil Darjean <akildarjean@protonmail.com>
2020-03-01 15:31:40 -0600

    Extract variables in components

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
-const AboutMe = () => (
-  <div class="section">
-    <h1 data-cy="about-me-heading">About Me</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id lacus eu enim tristique pulvinar. Aliquam hendrerit risus in quam porta ultricies. Duis eget nisl est. Nunc accumsan elementum neque non commodo. Nunc nec eros lacinia, hendrerit purus in, hendrerit purus. Curabitur vehicula tellus quam, in tincidunt lorem molestie non. Phasellus et augue sit amet tortor sollicitudin laoreet. Quisque rutrum, sem vel rhoncus cursus, dui nunc auctor orci, vel auctor lacus tellus non enim. Sed sit amet dui a dolor tempus sodales sit amet vel quam. Sed ac sapien non ex condimentum pellentesque. Nunc auctor magna orci, id interdum nulla ornare at.
-    </p>
-  </div>
-);
+const AboutMe = () => {
+  const heading = "About Me";
+  const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+  Etiam id lacus eu enim tristique pulvinar. Aliquam hendrerit risus in quam \
+  porta ultricies. Duis eget nisl est. Nunc accumsan elementum neque non \
+  commodo. Nunc nec eros lacinia, hendrerit purus in, hendrerit purus. \
+  Curabitur vehicula tellus quam, in tincidunt lorem molestie non. Phasellus \
+  et augue sit amet tortor sollicitudin laoreet. Quisque rutrum, sem vel \
+  rhoncus cursus, dui nunc auctor orci, vel auctor lacus tellus non enim. Sed \
+  sit amet dui a dolor tempus sodales sit amet vel quam. Sed ac sapien non ex \
+  condimentum pellentesque. Nunc auctor magna orci, id interdum nulla ornare at.";
+
+  return (
+    <div class="section">
+      <h1 data-cy="about-me-heading">{heading}</h1>
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default AboutMe;
--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -6,11 +6,16 @@ const Title = styled.h1`${tw`text-white`};`;
 const Subtitle = styled.h2`${tw`text-white`};`;
 const Border = styled.div`${tw`border-solid border-white border-2 lg:p-32 lg:py-8 p-16 py-4`};`;
 
-const MainHeading = () => (
-  <Border>
-    <Title data-cy="title">Akil Darjean</Title>
-    <Subtitle data-cy="subtitle">Software Developer</Subtitle>
-  </Border>
-);
+const MainHeading = () => {
+  const title = "Akil Darjean";
+  const subtitle = "Software Developer";
+
+  return (
+    <Border>
+      <Title data-cy="title">{title}</Title>
+      <Subtitle data-cy="subtitle">{subtitle}</Subtitle>
+    </Border>
+  );
+};
 
 export default MainHeading;
