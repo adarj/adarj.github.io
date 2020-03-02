cc4c8686bafcfce33ed051c7a4ad253f0d9b7218
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 15:52:33 -0600

    Reduce height of Main Heading component

--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -8,7 +8,7 @@ const Subtitle = styled.h2`${tw`text-white m-0 text-3xl`};`;
 const Border = styled.div`${tw`border-solid border-white border-3 lg:p-32 lg:py-8 p-6 py-4`};`;
 
 const MainHeading = (props) => (
-  <Hero imageSrc={props.image} color="#000" opacity={0.6} minHeight="100vh">
+  <Hero imageSrc={props.image} color="#000" opacity={0.6} minHeight="70vh">
     <Border>
       <Title data-cy="title">{props.title}</Title>
       <Subtitle data-cy="subtitle">{props.subtitle}</Subtitle>
