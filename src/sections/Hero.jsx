0220554c47224bbf9ef54b818b77d55af7afc1f0
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 15:53:55 -0600

    Use uppercase for Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import MainHeading from '../components/MainHeading';
 import heroBg from '../images/hero-bg.jpg';
 
-const title = "Akil Darjean";
-const subtitle = "Software Developer";
+const title = "AKIL DARJEAN";
+const subtitle = "SOFTWARE DEVELOPER";
 
 const Hero = () => (
   <MainHeading image={heroBg} title={title} subtitle={subtitle} />
