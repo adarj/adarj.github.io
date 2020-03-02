9268b803df4f7dbc21fbaf0c6a9e563dc2658395
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 14:43:25 -0600

    Fix Main Heading unit tests

--- src/components/MainHeading.test.jsx
+++ src/components/MainHeading.test.jsx
@@ -6,7 +6,7 @@ import heroBg from '../images/hero-bg.jpg';
 
 describe('Main Heading', () => {
   it('renders correctly', () => {
-    const tree = shallow(<MainHeading image={heroBg} title="John Doe" text="Lorem Ipsum" />);
+    const tree = shallow(<MainHeading image={heroBg} title="John Doe" subtitle="Lorem Ipsum" />);
     expect(tree).toMatchSnapshot();
   });
 });
