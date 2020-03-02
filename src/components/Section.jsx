7725e0c75808f5686e7a3cf5859bd8e76413c28a
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 14:11:40 -0600

    Remove React Hero from Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Hero from 'react-lazy-hero';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
@@ -7,10 +6,10 @@ const Heading = styled.h1`${tw`m-20`};`;
 const Text = styled.p`${tw`m-20`};`;
 
 const Section = (props) => (
-  <Hero opacity={0.0} minHeight="100vh">
+  <>
     <Heading data-cy="about-me-heading">{props.heading}</Heading>
     <Text data-cy="about-me-text">{props.text}</Text>
-  </Hero>
+  </>
 );
 
 export default Section;
