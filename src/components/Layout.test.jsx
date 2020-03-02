ab2516fc6fb8d9ec25dd4c0cf394579b7b72cebf
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 15:59:10 -0600

    Update unit tests

--- src/components/Layout.test.jsx
+++ src/components/Layout.test.jsx
@@ -5,7 +5,7 @@ import Layout from './Layout';
 
 describe('Layout', () => {
   it('renders correctly', () => {
-    const tree = shallow(<Layout />)
+    const tree = shallow(<Layout title="John Doe" />)
     expect(tree).toMatchSnapshot()
   });
 });
