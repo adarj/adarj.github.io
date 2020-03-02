88fbcafd79726a654d73b8e4365807fd32d88df7
Akil Darjean <akildarjean@protonmail.com>
2020-03-02 14:57:07 -0600

    Add 'Experience' section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,6 +7,7 @@ import Layout from '../components/Layout';
 // Sections
 import Hero from '../sections/Hero';
 import AboutMe from '../sections/AboutMe';
+import Experience from '../sections/Experience'
 
 const title = "Akil Darjean";
 
@@ -16,6 +17,7 @@ export default () => (
     <ReactPageScroller>
       <Hero />
       <AboutMe />
+      <Experience />
     </ReactPageScroller>
   </>
 );
