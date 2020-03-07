0089e72382965ec5308a559ff64a65792fd54dfe
Akil Darjean <akildarjean@protonmail.com>
2020-03-06 21:06:13 -0600

    Add icons to Main Heading component

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -10,6 +10,12 @@ describe('Home Page', () => {
     cy.get('[data-cy=subtitle]').contains('SOFTWARE DEVELOPER');
   });
 
+  it('Links resolve to correct address', () => {
+    cy.get('[data-cy=email-link]');
+    cy.get('[data-cy=github-link]');
+    cy.get('[data-cy=linkedin-link]');
+  });
+
   it('About Me displays correctly', () => {
     cy.get('[data-cy=about-me-heading]').contains('ABOUT ME');
   });
--- src/components/MainHeading.jsx
+++ src/components/MainHeading.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from 'react-lazy-hero';
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
 const Title = styled.h1`${tw`text-white m-0 mb-6 text-6xl font-normal`};`;
 const Subtitle = styled.h2`${tw`text-white m-0 text-3xl`};`;
 const Border = styled.div`${tw`border-solid border-white border-3 lg:p-32 lg:py-8 p-6 py-4`};`;
+const Links = styled.div`${tw``};`;
 
 const MainHeading = (props) => (
   <Hero imageSrc={props.image} color="#000" opacity={0.6} minHeight="70vh">
@@ -13,6 +15,11 @@ const MainHeading = (props) => (
       <Title data-cy="title">{props.title}</Title>
       <Subtitle data-cy="subtitle">{props.subtitle}</Subtitle>
     </Border>
+    <Links>
+      <FaEnvelope data-cy="email-link" />
+      <FaGithub data-cy="github-link" />
+      <FaLinkedin data-cy="linkedin-link" />
+    </Links>
   </Hero>
 );
 
