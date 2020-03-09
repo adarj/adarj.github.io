be47bd6f2113a6a339e97c3ed16cff154fd0b50e
Akil Darjean <akildarjean@protonmail.com>
2020-03-08 20:15:18 -0500

    Remove bottom margin in Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
-const Box = styled.div`${tw`mx-20 mt-20 mb-32`};`;
+const Box = styled.div`${tw`mx-20 mt-20`};`;
 
 const Section = (props) => (
   <Box>
