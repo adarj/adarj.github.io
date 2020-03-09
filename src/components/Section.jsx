36f5ee682c56809ade5ebdac9eac3ef339daef12
Akil Darjean <akildarjean@protonmail.com>
2020-03-08 23:19:15 -0500

    Rename 'Box' styled component to 'Container'

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import tw from 'tailwind.macro';
 
-const Box = styled.div`${tw`mx-20 mt-20`};`;
+const Container = styled.div`${tw`mx-20 mt-20`};`;
 
 const Section = (props) => (
-  <Box>
+  <Container>
     {props.children}
-  </Box>
+  </Container>
 );
 
 export default Section;
