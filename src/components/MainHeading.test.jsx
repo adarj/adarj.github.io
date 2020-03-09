c0692a3647ebff4d6303804eed986052281025b5
Akil Darjean <akildarjean@protonmail.com>
2020-03-08 22:58:36 -0500

    Add links to Main Heading test

--- src/components/MainHeading.test.jsx
+++ src/components/MainHeading.test.jsx
@@ -4,9 +4,18 @@ import { shallow } from 'enzyme';
 import MainHeading from './MainHeading';
 import heroBg from '../images/hero-bg.jpg';
 
+const links = {
+  email: "mailto:john.doe@mail.com",
+  github: "https://github.com",
+  linkedin: "https://linkedin.com"
+};
+
 describe('Main Heading', () => {
   it('renders correctly', () => {
-    const tree = shallow(<MainHeading image={heroBg} title="John Doe" subtitle="Lorem Ipsum" />);
+    const tree = shallow(<MainHeading image={heroBg}
+                                      title="John Doe"
+                                      subtitle="Lorem Ipsum"
+                                      {...links} />);
     expect(tree).toMatchSnapshot();
   });
 });
